c1242cfbc0fdad5be0fb18c6b2d92382aebd241e
hemanth-1188 <hk1100411004@gmail.com>
2026-01-19 20:45:32 +0530

    Update app.py

--- objectdeepseek/app.py
+++ objectdeepseek/app.py
@@ -52,6 +52,6 @@ def video_feed():
     # This is the "source" for our <img> tag in HTML
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-# At the bottom of app.py, change to:
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False, threaded=True)
